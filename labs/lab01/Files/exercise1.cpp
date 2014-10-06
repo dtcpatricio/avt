c07453ed6910ea633fa69c599f73693d56136f02
dtcpatricio <duarte.carvalho@ist.utl.pt>
2014-10-07 00:36:49 +0100

    First commit. Rotinas LookAt e Perspective concluidas. VertexShader tem agora duas matrizes out, uma para a projection e outra para a modelview.

---
+++ labs/lab01/Files/exercise1.cpp
@@ -0,0 +1,608 @@
+///////////////////////////////////////////////////////////////////////
+//
+// 
+// (c) 2014 by João Madeiras Pereira
+//
+///////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <math.h>
+#include <vector>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include "exercise1.h"
+#include "cube.h"
+
+#define CAPTION "Exercise 1"
+
+int WinX = 640, WinY = 480;
+int WindowHandle = 0;
+unsigned int FrameCount = 0;
+
+#define VERTEX_COORD_ATTRIB 0
+#define NORMAL_ATTRIB 1
+#define TEXTURE_COORD_ATTRIB 2
+
+#ifdef _WIN32
+#define M_PI       3.14159265358979323846f
+#endif
+
+GLuint VaoId, VboId[4];
+GLuint VertexShaderId, FragmentShaderId, ProgramId;
+GLuint UniformId, ProjId;
+
+GLfloat projMatrix[16];
+GLfloat viewMatrix[16];
+
+static inline float 
+DegToRad(float degrees) 
+{ 
+	return (float)(degrees * (M_PI / 180.0f));
+};
+
+/////////////////////////////////////////////////////////////////////// ERRORS
+
+bool isOpenGLError() {
+	bool isError = false;
+	GLenum errCode;
+	const GLubyte *errString;
+	while ((errCode = glGetError()) != GL_NO_ERROR) {
+		isError = true;
+		errString = gluErrorString(errCode);
+		std::cerr << "OpenGL ERROR [" << errString << "]." << std::endl;
+	}
+	return isError;
+}
+
+void checkOpenGLError(std::string error)
+{
+	if(isOpenGLError()) {
+		std::cerr << error << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+/////////////////////////////////////////////////////////////////////// SHADERs
+
+char *textFileRead(char *fn)
+{
+	FILE *fp;
+	char *content = NULL;
+
+	int count=0;
+
+	if (fn != NULL) {
+		fp = fopen(fn,"rt");
+
+		if (fp != NULL) {
+
+			fseek(fp, 0, SEEK_END);
+			count = ftell(fp);
+			rewind(fp);
+
+			if (count > 0) {
+				content = (char *)malloc(sizeof(char) * (count+1));
+				count = fread(content,sizeof(char),count,fp);
+				content[count] = '\0';
+			}
+			fclose(fp);
+		}
+	}
+	return content;
+}
+
+char * StringToCharPointer(std::string s)
+{
+	char * pointer = new char[s.size() + 1];
+	std::copy(s.begin(), s.end(), pointer);
+	pointer[s.size()] = '\0';
+
+	return pointer;
+}
+
+char* loadVertexShader()
+{
+	std::string vertexShader;
+	std::cin >> vertexShader;
+
+	char* fileName = StringToCharPointer(vertexShader);
+	return textFileRead(fileName);
+
+}
+
+char * loadFragmentShader()
+{
+	std::string fragmentShader;
+	std::cin >> fragmentShader;
+
+	char* fileName = StringToCharPointer(fragmentShader);
+	return textFileRead(fileName);
+}
+
+//const char * vertexShaderStr = loadVertexShader();
+const GLchar *VertexShader = //vertexShaderStr =
+{
+	"#version 150 core\n"
+
+	"in vec4 in_Position;\n"				
+	"uniform mat4 projMatrix, viewMatrix;\n"
+	"out vec4 color;\n"
+
+	"void main(void)\n"
+	"{\n"
+	"	color = in_Position;\n"
+	"	gl_Position = projMatrix * viewMatrix * in_Position;\n"
+
+	"}\n"
+};
+
+//const char * fragmentShaderStr = loadFragmentShader();
+const GLchar *FragmentShader =
+{
+	"#version 150 core\n"
+
+	"in vec4 color;\n"
+	"out vec4 out_Color;\n"
+
+	"void main(void)\n"
+	"{\n"
+	"	out_Color = color;\n"
+	"}\n"
+};
+
+void createShaderProgram()
+{
+	VertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(VertexShaderId, 1, &VertexShader, 0);
+	glCompileShader(VertexShaderId);
+
+	FragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(FragmentShaderId, 1, &FragmentShader, 0);
+	glCompileShader(FragmentShaderId);
+
+	ProgramId = glCreateProgram();
+	glAttachShader(ProgramId, VertexShaderId);
+	glAttachShader(ProgramId, FragmentShaderId);
+
+	glBindAttribLocation(ProgramId, VERTEX_COORD_ATTRIB, "in_Position");
+
+	glLinkProgram(ProgramId);
+	UniformId = glGetUniformLocation(ProgramId, "viewMatrix");
+	ProjId = glGetUniformLocation(ProgramId, "projMatrix");
+	
+	checkOpenGLError("ERROR: Could not create shaders.");
+}
+
+void destroyShaderProgram()
+{
+	glUseProgram(0);
+	glDetachShader(ProgramId, VertexShaderId);
+	glDetachShader(ProgramId, FragmentShaderId);
+
+	glDeleteShader(FragmentShaderId);
+	glDeleteShader(VertexShaderId);
+	glDeleteProgram(ProgramId);
+
+	checkOpenGLError("ERROR: Could not destroy shaders.");
+}
+
+/////////////////////////////////////////////////////////////////////// VAOs & VBOs
+
+
+void createBufferObjects()
+{
+	glGenVertexArrays(1, &VaoId);
+	glBindVertexArray(VaoId);
+
+	glGenBuffers(4, VboId);
+
+	//vertex coordinates buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VboId[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(VERTEX_COORD_ATTRIB);
+	glVertexAttribPointer(VERTEX_COORD_ATTRIB, 4, GL_FLOAT, 0, 0, 0);
+
+	//normals buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VboId[2]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(normals), normals, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(NORMAL_ATTRIB);
+	glVertexAttribPointer(NORMAL_ATTRIB, 3, GL_FLOAT, 0, 0, 0);
+
+	//texture coordinates buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VboId[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(texCoords), texCoords, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(TEXTURE_COORD_ATTRIB);
+	glVertexAttribPointer(TEXTURE_COORD_ATTRIB, 2, GL_FLOAT, 0, 0, 0);
+
+	//index buffer
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VboId[3]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(faceIndex), faceIndex, GL_STATIC_DRAW);
+
+	// unbind the VAO
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glDisableVertexAttribArray(VERTEX_COORD_ATTRIB);
+	glDisableVertexAttribArray(NORMAL_ATTRIB);
+	glDisableVertexAttribArray(TEXTURE_COORD_ATTRIB);
+
+	checkOpenGLError("ERROR: Could not create VAOs and VBOs.");
+}
+
+void destroyBufferObjects()
+{
+	glDisableVertexAttribArray(VERTEX_COORD_ATTRIB);
+	glDisableVertexAttribArray(NORMAL_ATTRIB);
+	glDisableVertexAttribArray(TEXTURE_COORD_ATTRIB);
+
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	glDeleteBuffers(4, VboId);
+	glDeleteVertexArrays(1, &VaoId);
+	checkOpenGLError("ERROR: Could not destroy VAOs and VBOs.");
+}
+
+/////////////////////////////////////////////////////////////////////// SCENE
+
+typedef GLfloat Matrix[16];
+
+const Matrix I = {
+	1.0f,  0.0f,  0.0f,  0.0f,
+	0.0f,  1.0f,  0.0f,  0.0f,
+	0.0f,  0.0f,  1.0f,  0.0f,
+	0.0f,  0.0f,  0.0f,  1.0f
+}; // Row Major (GLSL is Column Major)
+
+Matrix M = {
+	1.0f,  0.0f,  0.0f, 0.0f,
+	0.0f,  1.0f,  0.0f, 0.0f,
+	0.0f,  0.0f,  1.0f, 0.0f,
+	0.0f,  0.0f,  0.0f, 1.0f
+}; // Row Major (GLSL is Column Major)
+
+void renderScene()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glBindVertexArray(VaoId);
+	glUseProgram(ProgramId);
+
+	//translation(.5f, .5f, 0.0f);
+	
+	glUniformMatrix4fv(UniformId, 1, false, viewMatrix);
+	glUniformMatrix4fv(ProjId, 1, false, projMatrix);
+	glDrawElements(GL_TRIANGLES, faceCount*3, GL_UNSIGNED_INT, (GLvoid*)0);
+
+	glUseProgram(0);
+	glBindVertexArray(0);
+
+	checkOpenGLError("ERROR: Could not draw scene.");
+}
+
+/////////////////////////////////////////////////////////////////////// TRANSFORMATIONS
+
+// res = a cross b;
+void crossProduct(GLfloat *a, GLfloat *b, GLfloat *res) {
+
+	res[0] = a[1] * b[2]  -  b[1] * a[2];
+	res[1] = a[2] * b[0]  -  b[2] * a[0];
+	res[2] = a[0] * b[1]  -  b[0] * a[1];
+}
+
+
+// returns a . b
+float dotProduct(GLfloat *a, GLfloat *b) {
+
+	float res = a[0] * b[0]  +  a[1] * b[1]  +  a[2] * b[2];
+
+	return res;
+}
+
+void setIdentityMatrix( float *mat, int size) {
+
+	// fill matrix with 0s
+	for (int i = 0; i < size * size; ++i)
+			mat[i] = 0.0f;
+
+	// fill diagonal with 1s
+	for (int i = 0; i < size; ++i)
+		mat[i + i * size] = 1.0f;
+}
+
+// Normalize a vec3
+void normalize(float *a) {
+
+	float mag = sqrt(a[0] * a[0]  +  a[1] * a[1]  +  a[2] * a[2]);
+
+	a[0] /= mag;
+	a[1] /= mag;
+	a[2] /= mag;
+}
+
+void matrixMultiplication(Matrix m, Matrix aux)
+{
+	GLfloat res[16];
+	for(int i = 0; i < 4; i++)
+	{
+		for(int j = 0; j < 4; j++)
+		{
+			res[j*4 + i] = 0.0f;
+			for(int k = 0; k < 4; k++)
+			{
+				res[j*4 + i] += m[k*4 + i] * aux[j*4 + k];  
+			}
+		}
+	}
+	memcpy(m, res, 16 * sizeof(GLfloat));
+}
+
+void translation(float x, float y, float z)
+{
+	Matrix aux;
+	setIdentityMatrix(aux, 4);
+	aux[12] = x;
+	aux[13] = y;
+	aux[14] = z;
+
+	matrixMultiplication(M, aux);
+}
+
+void scale(float x, float y, float z){
+
+	Matrix aux = {
+		x,  0.0f,  0.0f, 0.0f,
+		0.0f,  y,  0.0f, 0.0f,
+		0.0f,  0.0f,  z, 0.0f,
+		0.0f,  0.0f,  0.0f,  1.0f
+	};
+
+	matrixMultiplication(M, aux);
+}
+
+void rotate(float angle, float x, float y, float z)
+{
+	GLfloat mat[16];
+	float v[3];
+
+	v[0] = x;
+	v[1] = y;
+	v[2] = z;
+
+	float radAngle = DegToRad(angle);
+	float co = cos(radAngle);
+	float si = sin(radAngle);
+	normalize(v);
+	float x2 = v[0]*v[0];
+	float y2 = v[1]*v[1];
+	float z2 = v[2]*v[2];
+
+//	mat[0] = x2 + (y2 + z2) * co; 
+	mat[0] = co + x2 * (1 - co);// + (y2 + z2) * co; 
+	mat[4] = v[0] * v[1] * (1 - co) - v[2] * si;
+	mat[8] = v[0] * v[2] * (1 - co) + v[1] * si;
+	mat[12]= 0.0f;
+	   
+	mat[1] = v[0] * v[1] * (1 - co) + v[2] * si;
+//	mat[5] = y2 + (x2 + z2) * co;
+	mat[5] = co + y2 * (1 - co);
+	mat[9] = v[1] * v[2] * (1 - co) - v[0] * si;
+	mat[13]= 0.0f;
+	   
+	mat[2] = v[0] * v[2] * (1 - co) - v[1] * si;
+	mat[6] = v[1] * v[2] * (1 - co) + v[0] * si;
+//	mat[10]= z2 + (x2 + y2) * co;
+	mat[10]= co + z2 * (1 - co);
+	mat[14]= 0.0f;
+	   
+	mat[3] = 0.0f;
+	mat[7] = 0.0f;
+	mat[11]= 0.0f;
+	mat[15]= 1.0f;
+
+	matrixMultiplication(M, mat);
+}
+
+void lookAt(float xPos, float yPos, float zPos,
+					float xLook, float yLook, float zLook,
+					float xUp, float yUp, float zUp)
+{
+	float dir[3], right[3], up[3];
+
+	up[0] = xUp;	up[1] = yUp;	up[2] = zUp;
+
+	dir[0] =  (xLook - xPos);
+	dir[1] =  (yLook - yPos);
+	dir[2] =  (zLook - zPos);
+	normalize(dir);
+
+	crossProduct(dir,up,right);
+	normalize(right);
+
+	crossProduct(right,dir,up);
+	normalize(up);
+
+	GLfloat m2[16];
+
+	viewMatrix[0]  = right[0];
+	viewMatrix[4]  = right[1];
+	viewMatrix[8]  = right[2];
+	viewMatrix[12] = 0.0f;
+
+	viewMatrix[1]  = up[0];
+	viewMatrix[5]  = up[1];
+	viewMatrix[9]  = up[2];
+	viewMatrix[13] = 0.0f;
+
+	viewMatrix[2]  = -dir[0];
+	viewMatrix[6]  = -dir[1];
+	viewMatrix[10] = -dir[2];
+	viewMatrix[14] =  0.0f;
+
+	viewMatrix[3]  = 0.0f;
+	viewMatrix[7]  = 0.0f;
+	viewMatrix[11] = 0.0f;
+	viewMatrix[15] = 1.0f;
+
+	setIdentityMatrix(m2,4);
+	m2[12] = -xPos;
+	m2[13] = -yPos;
+	m2[14] = -zPos;
+
+	matrixMultiplication(viewMatrix, m2);
+}
+
+void perspective(float fov, float ratio, float nearp, float farp)
+{
+	float f = 1.0f / tan (fov * (M_PI / 360.0f));
+
+	setIdentityMatrix(projMatrix, 4);
+
+	projMatrix[0] = f / ratio;
+	projMatrix[1 * 4 + 1] = f;
+	projMatrix[2 * 4 + 2] = (farp + nearp) / (nearp - farp);
+	projMatrix[3 * 4 + 2] = (2.0f * farp * nearp) / (nearp - farp);
+	projMatrix[2 * 4 + 3] = -1.0f;
+	projMatrix[3 * 4 + 3] = 0.0f;
+}
+
+/////////////////////////////////////////////////////////////////////// CALLBACKS
+
+void cleanup()
+{
+	destroyShaderProgram();
+	destroyBufferObjects();
+}
+
+void display()
+{
+	++FrameCount;
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	renderScene();
+	glutSwapBuffers();
+}
+
+void idle()
+{
+		glutPostRedisplay();
+}
+
+void reshape(int w, int h)
+{
+	float ratio;
+	WinX = w;
+	WinY = h;
+	glViewport(0, 0, WinX, WinY);
+	
+}
+
+void timer(int value)
+{
+	std::ostringstream oss;
+	oss << CAPTION << ": " << FrameCount << " FPS @ (" << WinX << "x" << WinY << ")";
+	std::string s = oss.str();
+	glutSetWindow(WindowHandle);
+	glutSetWindowTitle(s.c_str());
+	FrameCount = 0;
+	glutTimerFunc(1000, timer, 0);
+}
+
+/////////////////////////////////////////////////////////////////////// SETUP
+
+void setupCallbacks() 
+{
+	glutCloseFunc(cleanup);
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+	glutReshapeFunc(reshape);
+	glutTimerFunc(0,timer,0);
+}
+
+void setupOpenGL() {
+	std::cerr << "CONTEXT: OpenGL v" << glGetString(GL_VERSION) << std::endl;
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDepthMask(GL_TRUE);
+	glDepthRange(0.0, 1.0);
+	glClearDepth(1.0);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CCW);
+}
+
+void setupGLEW() {
+	glewExperimental = GL_TRUE;
+	GLenum result = glewInit() ; 
+	if (result != GLEW_OK) { 
+		std::cerr << "ERROR glewInit: " << glewGetString(result) << std::endl;
+		exit(EXIT_FAILURE);
+	} 
+	GLenum err_code = glGetError();
+	printf ("Vendor: %s\n", glGetString (GL_VENDOR));
+	printf ("Renderer: %s\n", glGetString (GL_RENDERER));
+	printf ("Version: %s\n", glGetString (GL_VERSION));
+	printf ("GLSL: %s\n", glGetString (GL_SHADING_LANGUAGE_VERSION));
+
+}
+
+void setupGLUT(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+
+	glutInitContextVersion(3, 2);
+	glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE,GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+
+	glutInitWindowSize(WinX, WinY);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	WindowHandle = glutCreateWindow(CAPTION);
+	if(WindowHandle < 1) {
+		std::cerr << "ERROR: Could not create a new rendering window." << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void init(int argc, char* argv[])
+{
+	setupGLUT(argc, argv);
+	setupGLEW();
+	setupOpenGL();
+	createShaderProgram();
+	createBufferObjects();
+	setupCallbacks();
+}
+
+int main(int argc, char* argv[])
+{
+	float ratio;
+	init(argc, argv);
+	lookAt(2.0f, 2.0f, 4.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f);
+	
+	ratio = (1.0f * WinX) / WinY;
+	perspective(53.13f, ratio, .1f, -5.0f);
+
+	//setIdentityMatrix(projMatrix, 4);
+	glutMainLoop();
+	
+	exit(EXIT_SUCCESS);
+}
+
+// (DEBUG) Prints a matrix
+void printMatrix(GLfloat* matrix)
+{
+	for (int i = 0; i < 16;)
+	{
+		std::cout << matrix[i] << " " << matrix[i+1] << " " << matrix[i+2] << " " << matrix[i+3] << std::endl;
+		i += 4;
+	}
+}
+///////////////////////////////////////////////////////////////////////
---
+++ labs/lab01/Files/exercise1.h
@@ -0,0 +1,8 @@
+
+void lookAt(float xPos, float yPos, float zPos,
+					float xLook, float yLook, float zLook,
+					float xUp, float yUp, float zUp);
+
+void translation(float x, float y, float z);
+
+void normalize(float *a);
