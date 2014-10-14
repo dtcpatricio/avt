38dda4dcf9e0a482d637686ef3c8dfaa43c77929
dtcpatricio <duarte.carvalho@ist.utl.pt>
2014-10-14 02:21:23 +0100

    Implemented the smooth frog movement
    	- Using boolean values set when a key is pressed;
    	- And in the renderScene call, update the values accordingly.
    
    Implemented the frog movement with the mouse
    	- Using an old implementation of the gluUnProject (translates viewport coords to world ones)
    	- And then using the mouse pointer position (when a button is held down) to create a vector 	with the frog coordinates.
    
    (Note: .vcxproj is in the commit to solve any tree file structure problems for the project)

--- assignment 1/Frogger/Frogger/Frogger.cpp
+++ assignment 1/Frogger/Frogger/Frogger.cpp
@@ -10,6 +10,7 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
+#include "Frogger.h"
 #include "Frog.h"
 #include "OurMathLib.h"
 #include "vsShaderLib.h"
@@ -24,6 +25,10 @@
 #define PERSPECTIVETOP 1
 #define PERSPECTIVE3RD 2
 
+#define SWAP_ROWS_DOUBLE(a, b) { double *_tmp = a; (a) = (b); (b) = _tmp; }
+#define SWAP_ROWS_FLOAT(a, b) { float *_tmp = a; (a) = (b); (b) = _tmp; }
+#define MAT(m, r, c) (m)[(c)* 4 + (r)]
+
 OurMathLib calc;
 VSShaderLib shader;
 VSResSurfRevLib mySurf;
@@ -36,12 +41,13 @@ int faceCount = 12;
 
 // camera type
 int camType = ORTHOGONAL;
-
+GLboolean leftPressed = false, rightPressed = false, upPressed = false, downPressed = false;
 const GLfloat FPS = 1000 / 60;
 unsigned int FrameCount = 0;
 
 GLfloat projMatrix[16], viewMatrix[16], model[16];
 GLfloat color[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+GLfloat mouseWorldP[3];
 
 GLuint viewMatrixId, projId, modelId, colorId;
 
@@ -253,7 +259,213 @@ void ortho(float left, float right,
 	projMatrix[3 * 4 + 2] = -(farp + nearp) / (farp - nearp);
 }
 
+void MultiplyMatrixByVector4by4OpenGL_FLOAT(float *resultvector, const float *matrix, const float *pvector)
+{
+	resultvector[0] = matrix[0] * pvector[0] + matrix[4] * pvector[1] + matrix[8] * pvector[2] + matrix[12] * pvector[3];
+	resultvector[1] = matrix[1] * pvector[0] + matrix[5] * pvector[1] + matrix[9] * pvector[2] + matrix[13] * pvector[3];
+	resultvector[2] = matrix[2] * pvector[0] + matrix[6] * pvector[1] + matrix[10] * pvector[2] + matrix[14] * pvector[3];
+	resultvector[3] = matrix[3] * pvector[0] + matrix[7] * pvector[1] + matrix[11] * pvector[2] + matrix[15] * pvector[3];
+}
 
+//This code comes directly from GLU except that it is for float
+int glhInvertMatrixf2(float *m, float *out)
+{
+	float wtmp[4][8];
+	float m0, m1, m2, m3, s;
+	float *r0, *r1, *r2, *r3;
+	r0 = wtmp[0], r1 = wtmp[1], r2 = wtmp[2], r3 = wtmp[3];
+	r0[0] = MAT(m, 0, 0), r0[1] = MAT(m, 0, 1),
+		r0[2] = MAT(m, 0, 2), r0[3] = MAT(m, 0, 3),
+		r0[4] = 1.0, r0[5] = r0[6] = r0[7] = 0.0,
+		r1[0] = MAT(m, 1, 0), r1[1] = MAT(m, 1, 1),
+		r1[2] = MAT(m, 1, 2), r1[3] = MAT(m, 1, 3),
+		r1[5] = 1.0, r1[4] = r1[6] = r1[7] = 0.0,
+		r2[0] = MAT(m, 2, 0), r2[1] = MAT(m, 2, 1),
+		r2[2] = MAT(m, 2, 2), r2[3] = MAT(m, 2, 3),
+		r2[6] = 1.0, r2[4] = r2[5] = r2[7] = 0.0,
+		r3[0] = MAT(m, 3, 0), r3[1] = MAT(m, 3, 1),
+		r3[2] = MAT(m, 3, 2), r3[3] = MAT(m, 3, 3),
+		r3[7] = 1.0, r3[4] = r3[5] = r3[6] = 0.0;
+	/* choose pivot - or die */
+	if (fabsf(r3[0]) > fabsf(r2[0]))
+		SWAP_ROWS_FLOAT(r3, r2);
+	if (fabsf(r2[0]) > fabsf(r1[0]))
+		SWAP_ROWS_FLOAT(r2, r1);
+	if (fabsf(r1[0]) > fabsf(r0[0]))
+		SWAP_ROWS_FLOAT(r1, r0);
+	if (0.0 == r0[0])
+		return 0;
+	/* eliminate first variable     */
+	m1 = r1[0] / r0[0];
+	m2 = r2[0] / r0[0];
+	m3 = r3[0] / r0[0];
+	s = r0[1];
+	r1[1] -= m1 * s;
+	r2[1] -= m2 * s;
+	r3[1] -= m3 * s;
+	s = r0[2];
+	r1[2] -= m1 * s;
+	r2[2] -= m2 * s;
+	r3[2] -= m3 * s;
+	s = r0[3];
+	r1[3] -= m1 * s;
+	r2[3] -= m2 * s;
+	r3[3] -= m3 * s;
+	s = r0[4];
+	if (s != 0.0) {
+		r1[4] -= m1 * s;
+		r2[4] -= m2 * s;
+		r3[4] -= m3 * s;
+	}
+	s = r0[5];
+	if (s != 0.0) {
+		r1[5] -= m1 * s;
+		r2[5] -= m2 * s;
+		r3[5] -= m3 * s;
+	}
+	s = r0[6];
+	if (s != 0.0) {
+		r1[6] -= m1 * s;
+		r2[6] -= m2 * s;
+		r3[6] -= m3 * s;
+	}
+	s = r0[7];
+	if (s != 0.0) {
+		r1[7] -= m1 * s;
+		r2[7] -= m2 * s;
+		r3[7] -= m3 * s;
+	}
+	/* choose pivot - or die */
+	if (fabsf(r3[1]) > fabsf(r2[1]))
+		SWAP_ROWS_FLOAT(r3, r2);
+	if (fabsf(r2[1]) > fabsf(r1[1]))
+		SWAP_ROWS_FLOAT(r2, r1);
+	if (0.0 == r1[1])
+		return 0;
+	/* eliminate second variable */
+	m2 = r2[1] / r1[1];
+	m3 = r3[1] / r1[1];
+	r2[2] -= m2 * r1[2];
+	r3[2] -= m3 * r1[2];
+	r2[3] -= m2 * r1[3];
+	r3[3] -= m3 * r1[3];
+	s = r1[4];
+	if (0.0 != s) {
+		r2[4] -= m2 * s;
+		r3[4] -= m3 * s;
+	}
+	s = r1[5];
+	if (0.0 != s) {
+		r2[5] -= m2 * s;
+		r3[5] -= m3 * s;
+	}
+	s = r1[6];
+	if (0.0 != s) {
+		r2[6] -= m2 * s;
+		r3[6] -= m3 * s;
+	}
+	s = r1[7];
+	if (0.0 != s) {
+		r2[7] -= m2 * s;
+		r3[7] -= m3 * s;
+	}
+	/* choose pivot - or die */
+	if (fabsf(r3[2]) > fabsf(r2[2]))
+		SWAP_ROWS_FLOAT(r3, r2);
+	if (0.0 == r2[2])
+		return 0;
+	/* eliminate third variable */
+	m3 = r3[2] / r2[2];
+	r3[3] -= m3 * r2[3], r3[4] -= m3 * r2[4],
+		r3[5] -= m3 * r2[5], r3[6] -= m3 * r2[6], r3[7] -= m3 * r2[7];
+	/* last check */
+	if (0.0 == r3[3])
+		return 0;
+	s = 1.0 / r3[3];		/* now back substitute row 3 */
+	r3[4] *= s;
+	r3[5] *= s;
+	r3[6] *= s;
+	r3[7] *= s;
+	m2 = r2[3];			/* now back substitute row 2 */
+	s = 1.0 / r2[2];
+	r2[4] = s * (r2[4] - r3[4] * m2), r2[5] = s * (r2[5] - r3[5] * m2),
+		r2[6] = s * (r2[6] - r3[6] * m2), r2[7] = s * (r2[7] - r3[7] * m2);
+	m1 = r1[3];
+	r1[4] -= r3[4] * m1, r1[5] -= r3[5] * m1,
+		r1[6] -= r3[6] * m1, r1[7] -= r3[7] * m1;
+	m0 = r0[3];
+	r0[4] -= r3[4] * m0, r0[5] -= r3[5] * m0,
+		r0[6] -= r3[6] * m0, r0[7] -= r3[7] * m0;
+	m1 = r1[2];			/* now back substitute row 1 */
+	s = 1.0 / r1[1];
+	r1[4] = s * (r1[4] - r2[4] * m1), r1[5] = s * (r1[5] - r2[5] * m1),
+		r1[6] = s * (r1[6] - r2[6] * m1), r1[7] = s * (r1[7] - r2[7] * m1);
+	m0 = r0[2];
+	r0[4] -= r2[4] * m0, r0[5] -= r2[5] * m0,
+		r0[6] -= r2[6] * m0, r0[7] -= r2[7] * m0;
+	m0 = r0[1];			/* now back substitute row 0 */
+	s = 1.0 / r0[0];
+	r0[4] = s * (r0[4] - r1[4] * m0), r0[5] = s * (r0[5] - r1[5] * m0),
+		r0[6] = s * (r0[6] - r1[6] * m0), r0[7] = s * (r0[7] - r1[7] * m0);
+	MAT(out, 0, 0) = r0[4];
+	MAT(out, 0, 1) = r0[5], MAT(out, 0, 2) = r0[6];
+	MAT(out, 0, 3) = r0[7], MAT(out, 1, 0) = r1[4];
+	MAT(out, 1, 1) = r1[5], MAT(out, 1, 2) = r1[6];
+	MAT(out, 1, 3) = r1[7], MAT(out, 2, 0) = r2[4];
+	MAT(out, 2, 1) = r2[5], MAT(out, 2, 2) = r2[6];
+	MAT(out, 2, 3) = r2[7], MAT(out, 3, 0) = r3[4];
+	MAT(out, 3, 1) = r3[5], MAT(out, 3, 2) = r3[6];
+	MAT(out, 3, 3) = r3[7];
+	return 1;
+}
+
+int glhUnProjectf(float winx, float winy, float winz, float *modelView, float *projection, int *viewport,
+	float *posx, float *posy, float *posz)
+{
+	//Transformation matrices
+	float m[16], A[16];
+	float in[4], out[4];
+	//Calculation for inverting a matrix, compute projection x modelview
+	//and store in A[16]
+	calc.setIdentityMatrix(A, 4);
+	calc.matrixMultiplication(A, modelView);
+	calc.matrixMultiplication(A, projection);
+	//Now compute the inverse of matrix A
+	if (glhInvertMatrixf2(A, m) == 0)
+		return 0;
+	//Transformation of normalized coordinates between -1 and 1
+	in[0] = (winx - (float)viewport[0]) / (float)viewport[2] * 2.0 - 1.0;
+	in[1] = (winy - (float)viewport[1]) / (float)viewport[3] * 2.0 - 1.0;
+	in[2] = 2.0*winz - 1.0;
+	in[3] = 1.0;
+	//Objects coordinates
+	MultiplyMatrixByVector4by4OpenGL_FLOAT(out, m, in);
+	if (out[3] == 0.0)
+		return 0;
+	out[3] = 1.0 / out[3];
+	*posx = out[0] * out[3];
+	*posy = out[1] * out[3];
+	*posz = - out[2] * out[3];
+	return 1;
+}
+void GetOGLPos(int x, int y)
+{
+	GLint viewport[4];
+	GLfloat winX, winY, winZ;
+	GLfloat posX, posY, posZ;
+
+	glGetIntegerv(GL_VIEWPORT, viewport);
+
+	winX = (float)x;
+	winY = (float)viewport[3] - (float)y;
+	glReadPixels(x, int(winY), 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &winZ);
+
+	glhUnProjectf(winX, winY, winZ, viewMatrix, projMatrix, viewport, &posX, &posY, &posZ);
+	mouseWorldP[0] = posX;
+	mouseWorldP[1] = posY;
+	mouseWorldP[2] = posZ;
+	std::cout << "Pos: " << posX << " " << posY << " " << posZ << std::endl;
+}
 /////////////////////////////////////////////////////////////////////// SCENE
 
 void createRiver(){
@@ -502,7 +714,7 @@ void applyTransformations(int objId) {
 			sendToGL(objId);
 			break;
 
-			//Car's  Note: Bozo Programming
+			//Car's wheels  Note: Bozo Programming
 		case 10:
 			for (int j = 0; j < 4; j++){
 				switch (j){
@@ -562,6 +774,7 @@ void renderScene()
 		break;
 	}
 	
+	updateFrog();
 	glUseProgram(shader.getProgramIndex());
 
 	glUniformMatrix4fv(viewMatrixId, 1, false, viewMatrix);
@@ -588,7 +801,7 @@ void renderScene()
 
 /////////////////////////////////////////////////////////////////////// Frog Movement Routines
 
-float frogSpeed = 0.2;
+float frogSpeed = 0.05 * 1 / 60;
 
 void frogLeft(){
 	float x = frog.getX();
@@ -622,6 +835,16 @@ void frogBackwards(){
 	frog.setZ(z + frogSpeed);
 }
 
+void updateFrog() {
+	if (leftPressed == true)
+		frogLeft();
+	if (rightPressed == true)
+		frogRight();
+	if (upPressed == true)
+		frogForward();
+	if (downPressed == true)
+		frogBackwards();
+}
 /////////////////////////////////////////////////////////////////////// CALLBACKS
 
 void cleanup()
@@ -716,7 +939,6 @@ void processMouseMotion(int xx, int yy)
 	}
 	// right mouse button: zoom
 	else if (tracking == 2) {
-
 		alphaAux = alpha;
 		betaAux = beta;
 		rAux = r + (deltaY * 0.01f);
@@ -740,8 +962,32 @@ void processMouseButtons(int button, int state, int xx, int yy)
 		startY = yy;
 		if (button == GLUT_LEFT_BUTTON)
 			tracking = 1;
-		else if (button == GLUT_RIGHT_BUTTON)
+		else if (button == GLUT_RIGHT_BUTTON) {
 			tracking = 2;
+			GetOGLPos(xx, yy);
+			float xcomp = mouseWorldP[0] - frog.getX();
+			float zcomp = mouseWorldP[2] - frog.getZ();
+			float xabs = abs(xcomp);
+			float zabs = abs(zcomp);
+			std::cout << "X: " << xcomp << std::endl << "Z: " << zcomp << std::endl;
+			if (xcomp >= 0 && zcomp <= 0) {
+				upPressed = true;
+				if (xabs >= zabs)
+					rightPressed = true;
+				else
+					leftPressed = true;
+			}
+			else {
+				downPressed = true;
+				if (xabs >= zabs)
+					leftPressed = true;
+				else
+					rightPressed = true;
+			}
+				
+
+			
+		}
 	}
 
 	//stop tracking the mouse
@@ -751,6 +997,7 @@ void processMouseButtons(int button, int state, int xx, int yy)
 			beta += (yy - startY);
 		}
 		else if (tracking == 2) {
+			upPressed = false; downPressed = false; leftPressed = false; rightPressed = false;
 			r += (yy - startY) * 0.01f;
 			if (r < 0.1f)
 				r = 0.1f;
@@ -760,7 +1007,7 @@ void processMouseButtons(int button, int state, int xx, int yy)
 }
 
 void processKeys(unsigned char key, int xx, int yy)
-{
+{	
 	switch(key) {
 
 	case '1':
@@ -779,19 +1026,23 @@ void processKeys(unsigned char key, int xx, int yy)
 		break;
 
 	case 'o':
-		frogLeft();
+		//frogLeft();
+		leftPressed = true;
 		break;
 
 	case 'p':
-		frogRight();
+		//frogRight();
+		rightPressed = true;
 		break;
 
 	case 'q':
-		frogForward();
+		//frogForward();
+		upPressed = true;
 		break;
 
 	case 'a':
-		frogBackwards();
+		//frogBackwards();
+		downPressed = true;
 		break;
 
 	case 27:
@@ -803,9 +1054,28 @@ void processKeys(unsigned char key, int xx, int yy)
 		break;
 
 	}
-
 //  uncomment this if not using an idle func
 //	glutPostRedisplay();
+}
+
+void processUpKeys(unsigned char key, int x, int y) {
+	switch (key) {
+	case 'o':
+		leftPressed = false;
+		break;
+
+	case 'p':
+		rightPressed = false;
+		break;
+
+	case 'q':
+		upPressed = false;
+		break;
+
+	case 'a':
+		downPressed = false;
+		break;
+	}
 }
 
 /////////////////////////////////////////////////////////////////////// SETUP
@@ -821,6 +1091,7 @@ void setupCallbacks()
 
 	//	Mouse and Keyboard Callbacks
 	glutKeyboardFunc(processKeys);
+	glutKeyboardUpFunc(processUpKeys);
 	glutMouseFunc(processMouseButtons);
 	glutMotionFunc(processMouseMotion);
 	glutMouseWheelFunc ( mouseWheel ) ;
